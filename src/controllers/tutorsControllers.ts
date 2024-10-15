82f0b0eae01696fe110396990f41fde8eaad650f
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-15 17:17:00 +0100

    refactor: remove argument from getTutorById in tutorsControllers.ts

--- src/controllers/tutorsControllers.ts
+++ src/controllers/tutorsControllers.ts
@@ -3,7 +3,7 @@ import { Response, Request } from "express";
 
 const getTutorController = (req: Request, res: Response) => {
   const { id } = req.params; // Extract the ID from the URL parameters
-  const tutor = getTutorById(Number(id)); // Convert to number if necessary
+  const tutor = getTutorById(); // Convert to number if necessary
 
   if (tutor) {
     res.json(tutor); // Send the tutor object as a JSON response
