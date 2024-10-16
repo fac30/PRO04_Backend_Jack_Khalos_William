c6cbb490b495633f04e132b162df3929519a054b
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-16 09:34:48 +0100

    chore: remove comments from tutorsControllers.ts

--- src/controllers/tutorsControllers.ts
+++ src/controllers/tutorsControllers.ts
@@ -2,13 +2,13 @@ import getTutorById from "../models/getTutor";
 import { Response, Request } from "express";
 
 const getTutorController = (req: Request, res: Response) => {
-  const { id } = req.params; // Extract the ID from the URL parameters
-  const tutor = getTutorById(); // Convert to number if necessary
+  const { id } = req.params; 
+  const tutor = getTutorById(); 
 
   if (tutor) {
-    res.json(tutor); // Send the tutor object as a JSON response
+    res.json(tutor); 
   } else {
-    res.status(404).json({ error: "Tutor not found" }); // Handle not found case
+    res.status(404).json({ error: "Tutor not found" }); 
   }
 };
 
