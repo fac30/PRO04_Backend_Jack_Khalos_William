b37ceb94e4acb0df6aa229ccd7f3c3a5a35f7062
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-16 09:57:00 +0100

    refactor: add types to getTutorController

--- src/controllers/tutorsControllers.ts
+++ src/controllers/tutorsControllers.ts
@@ -1,14 +1,14 @@
-import getTutorById from "../models/getTutor";
+import { getTutorById } from "../models/getTutor";
 import { Response, Request } from "express";
 
 const getTutorController = (req: Request, res: Response) => {
-  const { id } = req.params; 
-  const tutor = getTutorById(); 
+  const id: number = Number(req.params);
+  const tutor = getTutorById(id);
 
   if (tutor) {
-    res.json(tutor); 
+    res.json(tutor);
   } else {
-    res.status(404).json({ error: "Tutor not found" }); 
+    res.status(404).json({ error: "Tutor not found" });
   }
 };
 
