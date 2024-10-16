56f2e870253c9e94112cbf57ef0ed48d60467afd
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-16 12:24:30 +0100

    feat: install cors and configure in server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import tutorsRoutes from "./routes/tutorsRoutes";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 const port = 3000;
 
 app.get("/", (req, res) => {
