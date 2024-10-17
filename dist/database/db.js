a9984fa4c0ca575d028c82607f5efe52c3c2f3c0
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-17 11:48:32 +0100

    chore: run npm build

--- dist/database/db.js
+++ dist/database/db.js
@@ -1,8 +1,3 @@
 import Database from "better-sqlite3";
-// import { join } from "node:path";
-// import { readFileSync } from "node:fs";
-const zubiDB = new Database();
-console.log(zubiDB);
-// const schemaPath = join("database", "schema.sql");
-// const schema = readFileSync(schemaPath, "utf-8");
-// zubiDB
+const zubiDB = new Database("development.sqlite");
+export default zubiDB;
