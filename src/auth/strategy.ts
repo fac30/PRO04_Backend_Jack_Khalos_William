bcc79e8f6d031085bc60ec4477ae2a6aabeeb325
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-22 11:21:47 +0100

    chore: remove unused imports and comments

--- src/auth/strategy.ts
+++ src/auth/strategy.ts
@@ -2,7 +2,6 @@ import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
 import bcrypt from "bcrypt";
 import getStudentByEmail from "../models/getPassword";
-import { Express } from "express";
 
 declare global {
   namespace Express {
@@ -18,7 +17,7 @@ const loadStrategy = () => {
   passport.use(
     new LocalStrategy(
       {
-        usernameField: "email", // Tell passport to use email field
+        usernameField: "email",
       },
       async (email, password, cb) => {
         try {
@@ -44,12 +43,10 @@ const loadStrategy = () => {
     )
   );
 
-  // When we serialize, we only save the user id in the session
   passport.serializeUser((user: Express.User, done) => {
-    done(null, user.email); // Store email instead of id since getStudentByEmail uses email
+    done(null, user.email);
   });
 
-  // When we deserialize, we get the user by email
   passport.deserializeUser(async (email: string, done) => {
     try {
       const user = getStudentByEmail(email);
