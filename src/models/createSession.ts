9e10a1e6d00ff413ed6d12b4d61df91e4db9dd7e
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-22 12:08:13 +0100

    chore: update import in createsession.ts

--- src/models/createSession.ts
+++ src/models/createSession.ts
@@ -1,7 +1,6 @@
-import zubiDB from "src/database/db";
+import zubiDB from "../database/db";
 
 const insert = zubiDB.prepare(/*SQL*/ `
     INSERT INTO sessions (session_time, booking_status, fk_student_id, fk_tutor_id)
     VALUES (?, ?, ?, ?);
   `);
-    
