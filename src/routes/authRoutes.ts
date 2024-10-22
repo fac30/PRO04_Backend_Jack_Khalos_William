089eb056584e5acaee21eb3d89641de833dcbf18
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-22 10:26:31 +0100

    feat: add /auth route to login routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,8 +1,12 @@
 import express from "express";
-import { loginController } from "../controllers/authControllers";
+import {
+  loginController,
+  isAuthenticated,
+} from "../controllers/authControllers";
 
 const router = express.Router();
 
 router.post("/", (req, res, next) => loginController(req, res, next));
+router.post("/auth", isAuthenticated);
 
 export default router;
