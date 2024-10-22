a716fed7510430ec6c1b7a039bc97587386cc1b3
William Man <william.man@uymail.com>
2024-10-22 23:05:37 +0100

    fix: if statement to be more strict

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -4,7 +4,7 @@ import passport from "passport";
 const loginController = (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
 
-  if (!email || !password) {
+  if (!email && !password) {
     res.status(400).json({ message: "Email and password are required." });
     return;
   }
@@ -37,37 +37,37 @@ const isAuthenticated = (req: Request, res: Response, next: Function) => {
   res.status(401).json({ message: "Not authenticated" });
 };
 
-const logoutController = (req: Request, res: Response, next: NextFunction) =>{
-  if(!req.isAuthenticated()){
-    res.status(401).json({message: "Error logging out, user is not authenticated."})
-    return 
+const logoutController = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.isAuthenticated()) {
+    res
+      .status(401)
+      .json({ message: "Error logging out, user is not authenticated." });
+    return;
   }
 
-  req.logOut((err: unknown)=>{
-    if(err instanceof Error) {
-      res.status(500).json({message: err.message});
+  req.logOut((err: unknown) => {
+    if (err instanceof Error) {
+      res.status(500).json({ message: err.message });
       return;
     } else {
-      res.status(500).json({message: "Error occured during loging out."})
+      res.status(500).json({ message: "Error occured during loging out." });
       return;
     }
 
     req.session.destroy((err: unknown | null) => {
-      if(err instanceof Error) {
-        res.status(500).json({message: err.message})
+      if (err instanceof Error) {
+        res.status(500).json({ message: err.message });
         return;
       } else {
-        res.status(500).json({message:"Failed to end session."})
+        res.status(500).json({ message: "Failed to end session." });
         return;
       }
 
-      res.clearCookie('connect.sid', {path: '/'});
-      res.status(200).json({message: "Successfully logged out"});
+      res.clearCookie("connect.sid", { path: "/" });
+      res.status(200).json({ message: "Successfully logged out" });
       return;
     });
   });
 };
 
 export { loginController, isAuthenticated, logoutController };
-
-
