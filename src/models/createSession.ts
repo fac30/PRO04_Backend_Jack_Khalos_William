a26e98be8e1cda75a15e7df8ee98564e501ab81c
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-22 12:13:07 +0100

    feat: write createTutorAvailability function

--- src/models/createSession.ts
+++ src/models/createSession.ts
@@ -1,6 +1,13 @@
+import { create } from "domain";
 import zubiDB from "../database/db";
 
-const insert = zubiDB.prepare(/*SQL*/ `
+const insertSession = zubiDB.prepare(/*SQL*/ `
     INSERT INTO sessions (session_time, booking_status, fk_student_id, fk_tutor_id)
-    VALUES (?, ?, ?, ?);
+    VALUES (?, "open", NULL, ?);
   `);
+
+const createTutorAvailability = (sessionTime: string, tutorID: number) => {
+  insertSession.run(sessionTime, tutorID);
+};
+
+export default createTutorAvailability;
